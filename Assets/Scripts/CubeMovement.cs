dd27c6cf99b391d652b70cc65979227b2ef1da03
gozde-buse <gozde.buse.ocal@gmail.com>
2022-12-01 20:50:10 +0300

    Rotasyon kodları eklendi.

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -6,32 +6,31 @@ public class CubeMovement : MonoBehaviour
 {
     public float animationTime = 2f;
     public Vector3 newPos = Vector3.zero;
+    public Vector3 rotation = new Vector3(0, 180, 0);
 
     void Start()
     {
-        TranslateCube(newPos);
+        //TranslateCube(newPos);
+        RotateCube(rotation);
     }
 
-    private void Update()
+    public void TranslateCube(Vector3 targetDir)
     {
+        StartCoroutine(TranslateCubeCoroutine(targetDir));
     }
 
-    public void TranslateCube(Vector3 targetPos)
+    public void RotateCube(Vector3 rotAngles)
     {
-        StartCoroutine(TranslateCubeProcess(targetPos));
+        StartCoroutine(RotateCubeCoroutine(rotAngles));
     }
 
-    public void RotateCube()
-    {
-
-    }
-
-    IEnumerator TranslateCubeProcess(Vector3 targetPos)
+    IEnumerator TranslateCubeCoroutine(Vector3 targetDir)
     {
         float timeElapsed = 0;
         Vector3 initialPos = transform.position;
+        Vector3 targetPos = initialPos + targetDir;
 
-        while(timeElapsed < animationTime)
+        while (timeElapsed < animationTime)
         {
             transform.position = Vector3.Lerp(initialPos, targetPos, timeElapsed / animationTime);
             timeElapsed += Time.fixedDeltaTime;
@@ -40,5 +39,22 @@ public class CubeMovement : MonoBehaviour
         }
 
         transform.position = targetPos;
+    }
+
+    IEnumerator RotateCubeCoroutine(Vector3 rotAngles)
+    {
+        float timeElapsed = 0;
+        Vector3 initialRot = transform.eulerAngles;
+        Vector3 targetRot = initialRot + rotAngles;
+
+        while (timeElapsed < animationTime)
+        {
+            transform.eulerAngles = Vector3.Lerp(initialRot, targetRot, timeElapsed / animationTime);
+            timeElapsed += Time.fixedDeltaTime;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        transform.eulerAngles = targetRot;
     }
 }
